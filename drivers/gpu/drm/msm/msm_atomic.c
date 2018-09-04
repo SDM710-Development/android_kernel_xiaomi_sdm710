4b6e64735e0ed660e9ba8e9281c86d5424d0761d
Jayant Shekhar <jshekhar@codeaurora.org>
2018-09-04 21:09:42 +0530

    drm/msm: Trigger unblank event during bootup
    Touch driver needs DRM unblank event during bootup.
    Currently the unblank event is triggered only during
    suspend resume when userspace sets active property.
    So, trigger unblank during boot up as well when
    continuous splash is on. If continuous splash is off,
    userspace specifically sets active which triggers
    unblank event.
    
    Change-Id: Ia5db9a1233b938b0cf1c670838e4cc4a41bd234f
    Signed-off-by: Jayant Shekhar <jshekhar@codeaurora.org>
    Signed-off-by: Ivan Vecera <ivan@cera.cz>

--- drivers/gpu/drm/msm/msm_atomic.c
+++ drivers/gpu/drm/msm/msm_atomic.c
@@ -429,6 +429,7 @@ static void msm_atomic_helper_commit_modeset_enables(struct drm_device *dev,
 	struct msm_kms *kms = priv->kms;
 	int bridge_enable_count = 0;
 	int i, blank;
+	bool splash = false;
 
 	SDE_ATRACE_BEGIN("msm_enable");
 	for_each_crtc_in_state(old_state, crtc, old_crtc_state, i) {
@@ -481,8 +482,11 @@ static void msm_atomic_helper_commit_modeset_enables(struct drm_device *dev,
 		DRM_DEBUG_ATOMIC("enabling [ENCODER:%d:%s]\n",
 				 encoder->base.id, encoder->name);
 
-		if (connector->state->crtc &&
-			connector->state->crtc->state->active_changed) {
+		if (kms && kms->funcs && kms->funcs->check_for_splash)
+			splash = kms->funcs->check_for_splash(kms);
+
+		if (splash || (connector->state->crtc &&
+			connector->state->crtc->state->active_changed)) {
 			blank = MSM_DRM_BLANK_UNBLANK;
 			notifier_data.data = &blank;
 			notifier_data.id =
@@ -535,8 +539,9 @@ static void msm_atomic_helper_commit_modeset_enables(struct drm_device *dev,
 				 encoder->base.id, encoder->name);
 
 		drm_bridge_enable(encoder->bridge);
-		if (connector->state->crtc &&
-			connector->state->crtc->state->active_changed) {
+
+		if (splash || (connector->state->crtc &&
+			connector->state->crtc->state->active_changed)) {
 			DRM_DEBUG_ATOMIC("Notify unblank\n");
 			msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK,
 					    &notifier_data);
