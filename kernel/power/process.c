ab279821859c68079900c76075d87f6e926923f7
Alex Damaratski <alexeydomoratsky1@gmail.com>
2020-07-21 21:17:02 +0300

    drivers: power: supply: Import Xiaomi changes
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -95,8 +95,8 @@ static int try_to_freeze_tasks(bool user_only)
 		pr_cont("\n");
 		pr_err("Freezing of tasks aborted after %d.%03d seconds",
 		       elapsed_msecs / 1000, elapsed_msecs % 1000);
-	} else if (todo) {
-		pr_cont("\n");
+	}
+	 if (todo) {
 		pr_err("Freezing of tasks failed after %d.%03d seconds"
 		       " (%d tasks refusing to freeze, wq_busy=%d):\n",
 		       elapsed_msecs / 1000, elapsed_msecs % 1000,
