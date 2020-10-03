dafd7421ae979c94b22c31e3bc04bb8eaeb776d0
Albert I <kras@raphielgang.org>
2020-10-03 17:49:18 +0800

    power/qcom: smb2: Report battery technology as Li-Po
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -1098,7 +1098,7 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 					      FG_ESR_VOTER);
 		break;
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
-		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
+		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_DONE:
 		rc = smblib_get_prop_batt_charge_done(chg, val);
