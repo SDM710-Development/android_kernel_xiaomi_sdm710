9b899ceffaf647bf77efc8b99ef89e3bbc5c2dad
Albert I <kras@raphielgang.org>
2021-09-11 00:30:58 +0800

    Revert "[STOPSHIP]: thermal / core : Workaround for Xiaomi thermal mitigation"
    This reverts commit cee714b8b70ea9f70d6411e96681f3b6a6511ad8.
    
    Any attempt to write to sconfig with this change instead stalls the offending
    process, causing bootloop if it's done during init.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -81,7 +81,7 @@ struct screen_monitor {
 struct screen_monitor sm;
 #endif
 
-static atomic_t switch_mode = ATOMIC_INIT(10);
+static atomic_t switch_mode = ATOMIC_INIT(-1);
 static atomic_t temp_state = ATOMIC_INIT(0);
 static char boost_buf[128];
 
@@ -2688,12 +2688,12 @@ static ssize_t
 thermal_sconfig_store(struct device *dev, struct device_attribute *attr, const char *buf,
 		      size_t len)
 {
-	int rc, val;
+	int val = -1;
 
-	rc = kstrtoint(buf, 10, &val);
+	val = simple_strtol(buf, NULL, 10);
 	atomic_set(&switch_mode, val);
 
-	return rc;
+	return len;
 }
 static DEVICE_ATTR(sconfig, 0664, thermal_sconfig_show, thermal_sconfig_store);
 
@@ -2723,12 +2723,12 @@ static ssize_t
 thermal_temp_state_store(struct device *dev, struct device_attribute *attr, const char *buf,
 			 size_t len)
 {
-	int rc, val;
+	int val = -1;
 
-	rc = kstrtoint(buf, 10, &val);
+	val = simple_strtol(buf, NULL, 10);
 	atomic_set(&temp_state, val);
 
-	return rc;
+	return len;
 }
 static DEVICE_ATTR(temp_state, 0664, thermal_temp_state_show, thermal_temp_state_store);
 
